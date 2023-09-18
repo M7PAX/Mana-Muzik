7ec48ac71a9886de76ff0ed4c36cbffad49b562c
Up <123580378+M7PAX@users.noreply.github.com>
2023-09-19 00:07:35 +0300

    Update Program.py

--- Mana Muzik/Program.py
+++ Mana Muzik/Program.py
@@ -20,12 +20,11 @@ text = tk.Frame(editor)
 
 #top bar
 AutoB = tk.Label(TopBarF, text="Automation")
-DirectoryL = tk.Label(TopBarF, text="Music Directory")
+Divide = tk.Label(TopBarF, text="|")
+DirectoryL = tk.Label(TopBarF, text="Music Directory:")
 DirectoryE = tk.Entry(TopBarF, bd=3)
 ComfirmB = tk.Button(TopBarF, text ="✔")
 InfoB = tk.Label(TopBarF, text="INFO")
-
-
 
 def AutoClick(event):
     print("c")
@@ -33,10 +32,18 @@ def AutoClick(event):
 AutoB.bind("<Button-1>", AutoClick)
 
 
-#listbox
-listbox = tk.Listbox(FolderList, width=60)
+#Folder List Select 
+def FolderSelect():
+    tk.Label.config(text = clicked.get())
+options = ["Juice WRLD", "Playboi Carti"]
+
+scrollbar = tk.Scrollbar(FolderList)
+listbox = tk.Listbox(FolderList, width=60, yscrollcommand = scrollbar.set)
 for file in files:
     listbox.insert(tk.END, file)
+scrollbar.config(command = listbox.yview)
+
+scrollbar = tk.Scrollbar(FolderList)
 
 #editbox
 infobox = tk.Label(editor, text="x", bg="red")
@@ -54,14 +61,19 @@ image_label = tk.Label(window, image=photo, background="green")
 TopBarF.pack(side="top",fill="x")
 
 AutoB.pack(side="left",fill="both")
+Divide.pack(side="left",fill="both")
 DirectoryL.pack(side="left",fill="both")
 DirectoryE.pack(side="left",fill="both", expand=True)
 ComfirmB.pack(side="left",fill="both")
 InfoB.pack(side="left",fill="both")
 
-FolderList = tk.Frame(window)
+
+FolderList.pack(side="left",fill="y")
 
 listbox.pack(side="left",fill="y")
+scrollbar.pack( side = "left", fill = "y")
+
+
 #image_label.pack(side=tk.TOP, expand=True, fill=tk.BOTH, padx=10, pady=10)
 
 #infobox.pack(side=tk.LEFT, expand=True, fill=tk.BOTH, padx=10, pady=10)
@@ -70,11 +82,11 @@ listbox.pack(side="left",fill="y")
 
 
 #resize
-def on_window_resize(event):
-    new_height = event.height // 10
-    listbox.config(height=new_height)
+# def on_window_resize(event):
+#     new_height = event.height // 10
+#     listbox.config(height=new_height)
     
-window.bind("<Configure>", on_window_resize)
+# window.bind("<Configure>", on_window_resize)
 
 window.mainloop()
 
